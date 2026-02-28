aad1261faf9988cb0017591d0f4f8cfed185a4fd
sntgzrr <sntzrr@gmail.com>
2026-02-28 13:38:10 -0500

    scrollToTop func added

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ScrollToTop } from './utils/ScrollToTop'
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter> 
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,

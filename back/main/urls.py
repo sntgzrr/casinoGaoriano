481701772ec34931d36e9168fcd1d173f7698e35
sntgzrr <sntzrr@gmail.com>
2026-03-07 14:47:34 -0500

    renamed

--- back/main/urls.py
+++ back/main/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from news.views import hello
+from newsApp.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),

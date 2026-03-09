0d380efac2257d82d722a6283a8eb14c69b43ec4
sntgzrr <sntzrr@gmail.com>
2026-03-09 10:59:41 -0500

    products api added

---
+++ back/products/api.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, permissions
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ProductSerializer

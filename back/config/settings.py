473ee08c4b4a3a529dee2e8537389f50cba5062d
sntgzrr <sntzrr@gmail.com>
2026-03-10 10:34:46 -0500

    cookies auth added

--- back/config/settings.py
+++ back/config/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'apps.users.auth.cookies_auth.CookiesJWTAuthentication',
     ),
 }
 

df36e52e04625f3c53262eaafd7af9c3791a571f
sntgzrr <sntzrr@gmail.com>
2026-03-31 19:36:32 -0500

    env variables added to frontend

--- front/src/utils/Constants.js
+++ front/src/utils/Constants.js
@@ -1,4 +1,4 @@
-export const DOMAIN_BACK_NAME = 'http://localhost:8000' 
+export const DOMAIN_BACK_NAME = import.meta.env.VITE_API_URL
 export const LOGIN_URL = `${DOMAIN_BACK_NAME}/api/auth/token/`
 export const IS_AUTHENTICATED_URL = `${DOMAIN_BACK_NAME}/api/auth/authenticated/`
 export const IS_ADMIN_URL = `${DOMAIN_BACK_NAME}/api/auth/admin/`

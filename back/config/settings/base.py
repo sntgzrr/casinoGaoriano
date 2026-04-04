88da1a843a6e3a0d590cfba723f562b25fe3dcc9
sntgzrr <sntzrr@gmail.com>
2026-04-04 18:19:39 -0500

    testing CORS...

--- back/config/settings/base.py
+++ back/config/settings/base.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 # CORS CONFIGURATION
 # ============================================================================
 
-CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = env.bool('CORS_ALLOW_CREDENTIALS', default=True)
 
 

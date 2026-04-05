8fc1681151a145dc1244b9d277e1a16c134ac9a5
sntgzrr <sntzrr@gmail.com>
2026-04-04 19:24:27 -0500

    CORS fixed

--- back/config/settings/base.py
+++ back/config/settings/base.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 # CORS CONFIGURATION
 # ============================================================================
 
-CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[])
 CORS_ALLOW_CREDENTIALS = env.bool('CORS_ALLOW_CREDENTIALS', default=True)
 
 

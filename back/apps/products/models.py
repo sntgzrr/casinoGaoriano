60fed047a198956ff708abd327099832dc041b83
sntgzrr <sntzrr@gmail.com>
2026-06-11 11:22:20 -0500

    Price model data fixed

--- back/apps/products/models.py
+++ back/apps/products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product (models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.CharField(max_length=10)
     badge = models.CharField(max_length=20, blank=True)
     imageUrl = models.URLField(max_length=500)
     imageAlt = models.CharField(max_length=255)

57c6a3ed4c985b289b3ef57b4f45446c61b518c4
sntgzrr <sntzrr@gmail.com>
2026-06-11 15:07:11 -0500

    env prod added

--- back/config/settings/__init__.py
+++ back/config/settings/__init__.py
@@ -1,9 +1,2 @@
-import environ
-
-env = environ.Env()
-
 # Determine which settings file to use based on DEBUG environment variable
-if env.bool('DEBUG' == True, default=False):
-    from .dev import *
-else:
-    from .prod import *
+from .prod import *
